117e715001f1098a625f3b0a8630204a7faf94c4
tauno01 <tatu.malinen1@gmail.com>
2023-08-17 13:01:03 +0300

    Complete logic for the game (for now)
    The logic checks whether the user or opponent has 3 in a row
    squares marked, and then displays the winner. If the game ends
    in a tie, it displays 'Game ended in a tie'
    
    Player factory function is used to get the name, marker, and an array
    for both the player and opponent. This is a clean way to give both
    their own array, which is then later used to check if any of the
    winningIndexes match the player array inedexes
    
    Then the Player function calls Gamelogic functions for both user and
    opponent, and the Gamelogic factory function keeps track of the game
    progress

--- script.js
+++ script.js
@@ -1,49 +1,141 @@
 (function game() {
 
-    const gameboardArr = [];
     let stopOpponent = 0;
+    let gameboardArr = [];
+    let gameWon = false;
+    
+    const Player = (name, marker) => {
 
-    const Player = (marker) => {
+        const playerArr = [];
 
-        const playerIndex = (userMarker, cell) => {
-            cell.textContent = userMarker;
-            const cellValue = cell.getAttribute('value');
-            gameboardArr[cellValue] = userMarker;
+        const userIndex = (user, square) => {
+            if(!gameWon) {
+                const squareValue = square.getAttribute('value');
+                user.playerArr[+squareValue] = +squareValue;
+                Gamelogic.addToGameboard(user.marker, squareValue);
+                Gamelogic.displayMarker(user, square);
+            }
+        };
+
+        const opponentIndex = (opponent, gameboardSquare) => {
+            if(!gameWon) {
+                let randomNumber;
+                if(stopOpponent < 4) {
+                    do {
+                        randomNumber = Math.floor(Math.random() * 9);
+                    } while (gameboardArr[randomNumber] !== undefined);
+                
+                    opponent.playerArr[randomNumber] = randomNumber;
+                    Gamelogic.addToGameboard(opponent.marker, randomNumber);
+                    Gamelogic.displayMarker(opponent, gameboardSquare[randomNumber]);
+                    stopOpponent++;
+                } else return;
+            }
         }
 
-        const opponentIndex = (opponentMarker, gameboardCells) => {
-            stopOpponent++;
-            let randomIndex;
+        return { opponentIndex, userIndex, name, marker, playerArr, stopOpponent };
+    }
 
-            if(stopOpponent >= 5) return;
-           
-            do {
-                randomIndex = Math.floor(Math.random() * 9);
-            } while (gameboardArr[randomIndex] !== undefined);
-            gameboardArr[randomIndex] = opponentMarker;
-            gameboardCells[randomIndex].textContent = opponentMarker;
-            console.log(gameboardArr, randomIndex);
-            return stopOpponent;
+    const Gamelogic = (() => {
+      
+        const addToGameboard = (marker, squareValue) => {
+            gameboardArr[squareValue] = marker;
+        };
+
+        
+
+        const displayMarker = (player, square) => {
+            square.textContent = player.marker;
+            checkWinner(player.playerArr, player.name);
+        };
+
+        const checkWinner = (array, name) => {
+            winningIndexes = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ]
+
+            for(let i = 0; i < 8; i++) {
+                if(winningIndexes[i].every(index => array.includes(index))) {
+                    gameWon = true;
+                    displayWinner(name);
+                }
+            }
+
+            console.log(gameboardArr);
+
+            if(!gameWon && !gameboardArr.includes(undefined) && gameboardArr.length === 9) {
+                displayWinner('GameTie');
+            }
+
         }
 
-        return { marker, playerIndex, opponentIndex };
-    }
+        const displayWinner = (name) => {
+                const div = document.createElement('div');
+                div.setAttribute('id', 'display-div');
 
-    const user = Player('X');
-    const opponent = Player('0');
+                if(name !== 'GameTie') {
+                    div.textContent = `${name} won the game`;
+                } else div.textContent = 'Game ended in a tie';
 
-    const gameboardCells = document.getElementsByClassName('marker-spot');
-    for(const cell of gameboardCells) {
-        cell.addEventListener('click', () => {
-            if(cell.textContent !== '') return;
-            user.playerIndex(user.marker, cell);
-            opponent.opponentIndex(opponent.marker, gameboardCells);
-        })
-    }
+                div.style.color = 'white';
+                const containerDiv = document.getElementById('container');
+                containerDiv.appendChild(div);
+        };
 
-    (function winGame() {
-        const userWin = () => {};
-        const opponentWin = () => {};
-        return { userWin, opponentWin };
+        const resetGame = (user, opponent, gameboardSquare) => {
+            stopOpponent = 0;
+            opponent.playerArr = [];
+            user.playerArr = [];
+            gameboardArr = [];
+            for(let i = 0; i <= 8; i++) {
+                gameboardSquare[i].textContent = '';
+            }
+        }
+
+
+        return { displayMarker,
+                 addToGameboard,
+                 checkWinner,
+                 resetGame, 
+                 gameWon, 
+                 gameboardArr,
+                };
+
     })();
+
+
+    const startButton = document.getElementById('start-game');
+    startButton.addEventListener('click', () => {
+        startButton.disabled = true;
+        startButton.classList.add('disabled');
+        const userName = document.getElementById('user-name');
+        userName.disabled = true;
+        const user = Player(userName.value, 'X');
+        const opponent = Player('Opponent', '0');
+
+        const gameboardSquare = document.getElementsByClassName('marker-spot');
+        for(const square of gameboardSquare) {
+            square.addEventListener('click', () => {
+                if(square.textContent !== '') return;
+                user.userIndex(user, square);
+                opponent.opponentIndex(opponent, gameboardSquare);
+            });
+        };
+
+        const restartGame = document.getElementById('restart-game');
+        restartGame.addEventListener('click', () => {
+            gameWon = false;
+            Gamelogic.resetGame(user, opponent, gameboardSquare);
+            const div = document.getElementById('display-div');
+            div.remove();
+        })
+    });
+
 })();
